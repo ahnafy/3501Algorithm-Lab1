5383cc730be06cfa4ac841c6bef7e36435a944a3
Khondoker Yasin Ahnaf Prio <yasin011@morris.umn.edu>
2018-09-16 23:33:13 -0600

    finish permutation algorithm

--- src/Main.java
+++ src/Main.java
@@ -2,48 +2,52 @@ import java.util.Arrays;
 
 public class Main {
 
-    // copied from https://stackoverflow.com/questions/891031/is-there-a-method-that-calculates-a-factorial-in-java
-    public static long factorial(int number) {
-        long result = 1;
+    public static void makeNumbers(int col, Integer[] givenArr, Integer[] resultArr) {
 
-        for (int factor = 2; factor <= number; factor++) {
-            result *= factor;
+        if (col == resultArr.length) {
+            System.out.println(Arrays.toString(resultArr));
+            return;
         }
 
-        return result;
+        for (int i = 0; i < givenArr.length; i++) {
+            Integer num = i + 1;
+            if (!Arrays.asList(resultArr).contains(num)) {
+                resultArr[col] = givenArr[i];
+                Integer[] cloneResult = resultArr.clone();
+                makeNumbers(col + 1, givenArr, cloneResult);
+            }
+        }
     }
 
     public static void main(String[] args) {
 
         long time = System.currentTimeMillis();
-        int n = 3;
+        int n = 8;
 
+        Integer[] givenArr = new Integer[n];
+        Integer[] finalArr = new Integer[n];
 
-        long number = factorial(n-1);
-
-        for (int i = 1; i <= n; i++) {
-
-            for (int j = 0; j < number; j++) {
-                System.out.print(i);
-
-
-                System.out.print(i + 1);
-
-                System.out.println();
-
-
-/*                for (int l = 1; l <= n; l++) {
-                    if(i != l)
-                        System.out.print(l + " ");
-                }
-                System.out.println();*/
-
-            }
-
+        for (int i = 0; i < givenArr.length; i++) {
+            givenArr[i] = i + 1;
         }
 
+        makeNumbers(0, givenArr, finalArr);
 
         long endTime = System.currentTimeMillis();
         System.out.print("Time is: " + (endTime - time));
     }
 }
+
+/*
+* ANSWERS
+* The efficiency of the algorithm in the number of printed lines is f(n) = O(n!)
+* The number of lines when n=3 is 6, which is 3!, and the number of lines when n=4 is 24,
+* which is 4!, so we concluded that the number of lines is always n!.
+*
+* Based on the timing results, the function for time in milliseconds is n!/(2^n).
+*
+* So n=15 would take 39907054.6 milliseconds, which is about 11 hours.
+* n=20 would take 2.3201962 x 10^12 milliseconds, which is about 73.6 years
+*
+*
+*/
